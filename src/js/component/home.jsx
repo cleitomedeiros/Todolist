04c49f2ce931475064499e9e5a741a141f0dc0b3
cleitomedeiros <cleitovertical@gmail.com>
2023-03-06 19:48:05 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,102 @@
-import React from "react";
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+function App() {
+  const [tasks, setTasks] = useState([]);
 
-export default Home;
+  const addTask = (title) => {
+    const newTask = {
+      id: uuidv4(),
+      title: title,
+      completed: false
+    };
+    setTasks([...tasks, newTask]);
+  };
+
+  const deleteTask = (id) => {
+    const updatedTasks = tasks.filter(task => task.id !== id);
+    setTasks(updatedTasks);
+  };
+
+  const toggleComplete = (id) => {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <h1>To Do List</h1>
+      <TaskForm addTask={addTask} />
+      <TaskList tasks={tasks} deleteTask={deleteTask} toggleComplete={toggleComplete} />
+    </div>
+  );
+}
+
+function TaskForm({ addTask }) {
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTask(title);
+    setTitle('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+      <button type="submit">Add Task</button>
+    </form>
+  );
+}
+
+function TaskList({ tasks, deleteTask, toggleComplete }) {
+  return (
+    <ul>
+      {tasks.map(task => (
+        <li key={task.id}>
+          <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.title}</span>
+          <button onClick={() => deleteTask(task.id)} className="delete-task ">X</button>
+
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+
+export default App;
+
+// import React from "react";
+
+// //include images into your bundle
+// import rigoImage from "../../img/rigo-baby.jpg";
+
+// //create your first component
+// const Home = () => {
+// 	return (
+// 		<div className="text-center">
+// 			<h1 className="text-center mt-5">TODO REACT</h1>
+// 			<p>
+// 				<img src={rigoImage} />
+// 			</p>
+// 			<a href="#" className="btn btn-success">
+// 				If you see this green button... bootstrap is working...
+// 			</a>
+// 			<p>
+// 				Made by{" "}
+// 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+// 				love!
+// 			</p>
+			
+// 		</div>
+		
+// 	);
+// };
+
+// export default Home;
